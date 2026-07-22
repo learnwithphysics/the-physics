4eb05b5b6c08811e9795ac1c0cb9bc2097fd1d17
Vishal <vishallucky58@gmail.com>
2026-07-22 13:36:09 +0530

    Added student database

--- login.js
+++ login.js
@@ -1,14 +1,14 @@
 import { auth } from "./firebase.js";
+import { database } from "./firebase.js";
 
 import {
 createUserWithEmailAndPassword,
-signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
-
-import {
+signInWithEmailAndPassword,
 GoogleAuthProvider,
 signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+import { ref, set } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -60,11 +60,30 @@ msg.innerHTML = error.message;
 const provider = new GoogleAuthProvider();
 
 document.getElementById("googleLogin").addEventListener("click", async () => {
-  try {
-    await signInWithPopup(auth, provider);
-   window.location.href = "student.html";
-  } catch (error) {
-    msg.style.color = "red";
-    msg.innerHTML = error.message;
-  }
+
+    try {
+
+        const result = await signInWithPopup(auth, provider);
+
+        const user = result.user;
+
+        await set(ref(database, "students/" + user.uid), {
+
+            name: user.displayName,
+            email: user.email,
+            uid: user.uid,
+            photo: user.photoURL,
+            loginTime: new Date().toLocaleString()
+
+        });
+
+        window.location.href = "student.html";
+
+    } catch (error) {
+
+        msg.style.color = "red";
+        msg.innerHTML = error.message;
+
+    }
+
 });
